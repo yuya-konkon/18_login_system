d62c1203105bde939b84432c4ad068d1cd96354b
yuya-konkon <yuuuuya08@icloud.com>
2020-02-17 15:53:40 +0900

    fix

--- login.php
+++ login.php
@@ -54,12 +54,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ログイン画面</title>
-  <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/3ae6904195.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <h5 class="card-title text-center">Sign In</h5>
             <!-- エラーの表示 -->
             <?php if ($errors) : ?>
-              <ul class="error-list">
+              <ul class="alert alert-danger">
                 <?php foreach ($errors as $error) : ?>
                   <li><?php echo $error; ?></li>
                 <?php endforeach; ?>
@@ -87,10 +87,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="password">
                 <label for="inputPassword">Password</label>
               </div>
-
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+              <div class="form-label-group">
+                <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                <a href="signup.php" class="btn btn-lg btn-success btn-block text-uppercas">Create Account</a>
+              </div>
             </form>
-            <a href="signup.php">新規ユーザー登録はこちら</a>
           </div>
         </div>
       </div>
